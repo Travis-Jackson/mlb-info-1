f4c3f58299c6e070d7c1cb85464cedf40a28925f
Mario Jr <mariosanchezjr12@gmail.com>
2022-05-09 17:03:43 -0700

    added conditions to prevent repeated api calls

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // select main div
 const teamDiv = document.getElementById("teams");
+// input
 const season = document.getElementById("season");
-
+// contains input and get teams and reset buttons
 const buttonDiv = document.getElementById("buttons");
-
+// get teams
 const button = document.createElement("button");
 
 const reset = document.createElement("button");
@@ -14,11 +15,55 @@ reset.innerText = "Reset";
 buttonDiv.appendChild(button);
 buttonDiv.appendChild(reset);
 
+// search history array
+const history = [];
+
+// history heading
+const historyHeading = document.createElement("h3");
+historyHeading.innerText = "Search History:";
+buttonDiv.appendChild(historyHeading);
+// history paragraph
+const historyPara = document.createElement("p");
+buttonDiv.appendChild(historyPara);
+
 button.addEventListener("click", () => {
   if (season.value === "") {
     alert("Please Enter a Valid Year!");
   }
-  getTeamData(showList, season.value);
+
+  let yearPre;
+  let year;
+
+  if (history.length !== 0) {
+    // isolate year
+    yearPre = history[history.length - 1].split(">")[1];
+    year = yearPre.split("<")[0];
+  }
+  // check if the same year is being searched repeatedly
+  if (
+    year !== season.value ||
+    document.getElementsByClassName("team").length === 0
+  ) {
+    getTeamData(showList, season.value);
+
+    // add to search history
+    if (history[history.length - 1] !== `<a href="#">${season.value}</a>`) {
+      history.push(`<a href="#">${season.value}</a>`);
+      console.log(history);
+      historyPara.innerHTML = history;
+    }
+
+    const aTags = document.getElementsByTagName("a");
+    for (let i = 0; i < aTags.length; i++) {
+      aTags[i].addEventListener("click", () => {
+        const yearPre = history[i].split(">")[1];
+        const year = yearPre.split("<")[0];
+
+        season.value = year;
+        button.click();
+      });
+    }
+  }
 });
 
 reset.addEventListener("click", () => {
@@ -35,9 +80,9 @@ function showList(array) {
     teamDataDiv.setAttribute("id", "MLB-" + array[i]["team_id"]);
     teamDiv.appendChild(teamDataDiv);
 
+    const hrElem = document.createElement("hr");
     const teamName = document.createElement("h4");
     const teamVenue = document.createElement("p");
-    const hrElem = document.createElement("hr");
 
     teamName.innerText = array[i]["name_display_full"];
     teamVenue.innerText = "Venue: " + array[i]["venue_name"];
@@ -46,16 +91,25 @@ function showList(array) {
     teamDataDiv.appendChild(teamVenue);
 
     const showRosterButton = document.createElement("button");
+    showRosterButton.setAttribute("id", "btn-" + array[i]["team_id"]);
     showRosterButton.innerText = "Show Roster";
     teamDataDiv.appendChild(showRosterButton);
 
     // add event listener to each new div
     showRosterButton.addEventListener("click", () => {
       const teamId = teamDataDiv.id.split("-")[1];
+      // prevent more than one click
+      showRosterButton.disabled = true;
+      
+      // prevent more than one roster table
+      if (document.getElementById(teamDataDiv.id + "-table") !== null) {
+        return;
+      } 
 
       getRosterData(showRoster, season.value, teamId, teamDataDiv.id);
+     
     });
-
+    
     if (i !== array.length - 1) {
       teamDataDiv.appendChild(hrElem);
     }
@@ -82,8 +136,12 @@ async function getTeamData(passedFunc, year) {
 }
 
 function showRoster(array, divId) {
-  const rosterTable = document.createElement("table");
+
   const parentDiv = document.getElementById(divId);
+  
+  const rosterTable = document.createElement("table");
+  rosterTable.setAttribute("id", divId + "-table");
+                           
   parentDiv.appendChild(rosterTable);
 
   const hbDiv = document.createElement("div");
@@ -100,6 +158,10 @@ function showRoster(array, divId) {
     while (rosterTable.hasChildNodes()) {
       rosterTable.removeChild(rosterTable.firstChild);
     }
+
+    rosterTable.remove();  
+    // re-enable button
+    document.getElementById("btn-" + divId.split("-")[1]).disabled = false;
   });
 
   const tableHeaderRow = document.createElement("tr");
@@ -141,12 +203,13 @@ async function getRosterData(passedFunc, year, teamId, divId) {
   const url = `https://lookup-service-prod.mlb.com/json/named.roster_team_alltime.bam?start_season=${year}&end_season=${year}&team_id=${teamId}`;
   const response = await fetch(url, { method: "GET" });
   const data = await response.json();
-  const results = data["roster_team_alltime"]["queryResults"]["row"];
-  console.log(results);
-
+  const results = data["roster_team_alltime"]["queryResults"]["row"]; 
+ 
   if (results === undefined) {
     alert("Roster Not Available!");
   }
-
+  
   passedFunc(results, divId);
 }
+
+// new stuff!!
